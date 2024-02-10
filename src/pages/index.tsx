f7052975a55b47f2613f7158604823b14ffd3f5d
Heliya Rahbar <zahrarahbar1991@gmail.com>
2024-02-11 00:57:43 +0330

    Initial commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import { ConnectButton } from "@rainbow-me/rainbowkit";
+
+export default function Page() {
+  return (
+    <div>
+      <main>
+        <h1>Basic Next.js App</h1>
+        <ConnectButton />
+      </main>
+    </div>
+  );
+}
