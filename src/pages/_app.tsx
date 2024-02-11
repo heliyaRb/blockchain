70d0812a51f39a9c6053b94b2a5f2f4a454a07c0
Heliya Rahbar <zahrarahbar1991@gmail.com>
2024-02-11 15:31:15 +0330

    chore: update project ID

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,20 +1,20 @@
-import "@rainbow-me/rainbowkit/styles.css";
-import { getDefaultWallets, RainbowKitProvider } from "@rainbow-me/rainbowkit";
-import { configureChains, createConfig, WagmiConfig } from "wagmi";
-import { mainnet, polygon, optimism, arbitrum, base, zora } from "wagmi/chains";
-import { alchemyProvider } from "wagmi/providers/alchemy";
-import { publicProvider } from "wagmi/providers/public";
+import '@rainbow-me/rainbowkit/styles.css';
+import { getDefaultWallets, RainbowKitProvider } from '@rainbow-me/rainbowkit';
+import { configureChains, createConfig, WagmiConfig } from 'wagmi';
+import { mainnet, polygon, optimism, arbitrum, base, zora } from 'wagmi/chains';
+import { alchemyProvider } from 'wagmi/providers/alchemy';
+import { publicProvider } from 'wagmi/providers/public';
 
-import type { AppProps } from "next/app";
+import type { AppProps } from 'next/app';
 
 const { chains, publicClient } = configureChains(
   [mainnet, polygon, optimism, arbitrum, base, zora],
-  [alchemyProvider({ apiKey: process.env.ALCHEMY_ID! }), publicProvider()]
+  [alchemyProvider({ apiKey: process.env.ALCHEMY_ID! }), publicProvider()],
 );
 
 const { connectors } = getDefaultWallets({
-  appName: "My RainbowKit App",
-  projectId: "YOUR_PROJECT_ID",
+  appName: 'My RainbowKit App',
+  projectId: process.env.NEXT_PUBLIC_APP_PROJECT_ID as string,
   chains,
 });
 
@@ -24,7 +24,6 @@ const wagmiConfig = createConfig({
   publicClient,
 });
 
-//
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <WagmiConfig config={wagmiConfig}>
